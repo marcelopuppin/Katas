4f77683475eb525db87441e031971174a42f38a3
Marcelo Puppin <mhpuppin@hotmail.com>
2011-10-03 14:07:57 +0200

    Kata Tennis

---
+++ Tennis/TennisGame.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Katas
+{
+    public class TennisGame
+    {
+        private readonly int[] _pointsPlayer = new[] {0, 0};
+        
+        public string Score()
+        {
+            int pointsBetweenPlayers = Points(Player.One) - Points(Player.Two);
+            if (Points(Player.One) == 0 && pointsBetweenPlayers == 0)
+                return "love";
+            if (BothPlayersMadeAtLeastThreePoints() && pointsBetweenPlayers == 0)
+                return "deuce";
+            if (BothPlayersMadeAtLeastThreePoints() && Math.Abs(pointsBetweenPlayers) == 1)
+                return string.Format("advantage player {0}", (pointsBetweenPlayers > 0) ? "one" : "two");
+            if (APlayerMadeAtLeastFourPoints() && Math.Abs(pointsBetweenPlayers) > 1)
+                return string.Format("wins player {0}", (pointsBetweenPlayers > 0) ? "one" : "two");
+            return string.Format("{0}-{1}", GetScoreBy(Player.One), GetScoreBy(Player.Two));
+        }
+
+        private bool APlayerMadeAtLeastFourPoints()
+        {
+            return Points(Player.One) > 3 || Points(Player.Two) > 3;
+        }
+
+        private bool BothPlayersMadeAtLeastThreePoints()
+        {
+            return Points(Player.One) >= 3 && Points(Player.Two) >= 3;
+        }
+
+        public void MakesAPoint(Player player)
+        {
+            int index = IndexByPlayer(player);
+            _pointsPlayer[index]++;
+        }
+
+        private int Points(Player player)
+        {
+            int index = IndexByPlayer(player);
+            return _pointsPlayer[index];
+        }
+
+        private static int IndexByPlayer(Player player)
+        {
+            if (player == Player.One)
+            {
+                return 0;
+            }
+            return 1;
+        } 
+
+        private string GetScoreBy(Player player)
+        {
+            switch(Points(player))
+            {
+                case 1:
+                    return "fifteen";
+                case 2:
+                    return "thirty";
+                case 3:
+                    return "forty";
+                default:
+                    return "love";
+            }
+        }
+    }
+
+    public enum Player
+    {
+        One,
+        Two
+    }
+}
---
+++ Tennis/TennisTests.cs
@@ -0,0 +1,169 @@
+﻿using NUnit.Framework;
+
+namespace Katas
+{
+    [TestFixture]
+    public class TennisTests
+    {
+        [Test]
+        public void Given2Players_When_GameIsStarting_Then_ScoreIs_love()
+        {
+            var game = new TennisGame();
+            Assert.That(game.Score(), Is.EqualTo("love"));
+        }
+
+        [Test]
+        public void Given2Players_When_Player1MakesAPoint_Then_ScoreIs_fifteen_love()
+        {
+            var game = new TennisGame();
+            game.MakesAPoint(Player.One);
+            Assert.That(game.Score(), Is.EqualTo("fifteen-love"));
+        }
+
+        [Test]
+        public void Given2Players_When_Player1MakesTwoPoints_Then_ScoreIs_thirty_love()
+        {
+            var game = new TennisGame();
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.One);
+            Assert.That(game.Score(), Is.EqualTo("thirty-love"));
+        }
+
+        [Test]
+        public void Given2Players_When_Player1MakesThreePoints_Then_ScoreIs_forty_love()
+        {
+            var game = new TennisGame();
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.One);
+            Assert.That(game.Score(), Is.EqualTo("forty-love"));
+        }
+
+        [Test]
+        public void Given2Players_When_Player1MakesThreePointsAndPlayer2MakesOnePoint_Then_ScoreIs_forty_fifteen()
+        {
+            var game = new TennisGame();
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.Two);
+            Assert.That(game.Score(), Is.EqualTo("forty-fifteen"));
+        }
+
+        [Test]
+        public void Given2Players_When_Player1MakesThreePointsAndPlayer2MakesTwoPoints_Then_ScoreIs_forty_thirty()
+        {
+            var game = new TennisGame();
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.Two);
+            game.MakesAPoint(Player.Two);
+            Assert.That(game.Score(), Is.EqualTo("forty-thirty"));
+        }
+
+        [Test]
+        public void Given2Players_When_Player1MakesTwoPointsAndPlayer2MakesTwoPoints_Then_ScoreIs_thirty_thirty()
+        {
+            var game = new TennisGame();
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.Two);
+            game.MakesAPoint(Player.Two);
+            Assert.That(game.Score(), Is.EqualTo("thirty-thirty"));
+        }
+
+        [Test]
+        public void Given2Players_When_Player1MakesThreePointsAndPlayer2MakesThreePoints_Then_ScoreIs_deuce()
+        {
+            var game = new TennisGame();
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.Two);
+            game.MakesAPoint(Player.Two);
+            game.MakesAPoint(Player.Two);
+            Assert.That(game.Score(), Is.EqualTo("deuce"));
+        }
+
+        [Test]
+        public void Given2Players_When_Player1MakesFourPointsAndPlayer2MakesThreePointsAlternately_Then_ScoreIs_advantage_player_one()
+        {
+            var game = new TennisGame();
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.Two);
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.Two);
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.Two);
+            game.MakesAPoint(Player.One);
+            Assert.That(game.Score(), Is.EqualTo("advantage player one"));
+        }
+
+        [Test]
+        public void Given2Players_When_Player1MakesThreePointsAndPlayer2MakesFourPointsAlternately_Then_ScoreIs_advantage_player_two()
+        {
+            var game = new TennisGame();
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.Two);
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.Two);
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.Two);
+            game.MakesAPoint(Player.Two);
+            Assert.That(game.Score(), Is.EqualTo("advantage player two"));
+        }
+
+        [Test]
+        public void Given2Players_When_Player1MakesFivePointsAndPlayer2MakesThreePointsAlternately_Then_ScoreIs_wins_player_one()
+        {
+            var game = new TennisGame();
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.Two);
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.Two);
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.Two);
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.One);
+            Assert.That(game.Score(), Is.EqualTo("wins player one"));
+        }
+
+        [Test]
+        public void Given2Players_When_Player1MakesThreePointsAndPlayer2MakesFivePointsAlternately_Then_ScoreIs_wins_player_two()
+        {
+            var game = new TennisGame();
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.Two);
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.Two);
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.Two);
+            game.MakesAPoint(Player.Two);
+            game.MakesAPoint(Player.Two);
+            Assert.That(game.Score(), Is.EqualTo("wins player two"));
+        }
+
+        [Test]
+        public void Given2Players_When_Player1MakesFourPointsDirectly_Then_ScoreIs_wins_player_one()
+        {
+            var game = new TennisGame();
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.One);
+            Assert.That(game.Score(), Is.EqualTo("wins player one"));
+        }
+
+        [Test]
+        public void Given2Players_When_Player2MakesFourPointsDirectly_Then_ScoreIs_wins_player_two()
+        {
+            var game = new TennisGame();
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.One);
+            game.MakesAPoint(Player.One);
+            Assert.That(game.Score(), Is.EqualTo("wins player one"));
+        }
+    }
+}
